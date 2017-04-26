af6efa7790ba1c3b729b868a72b34a0f2b2448f0
MrLister <rlister@wonkasoft.com>
2017-04-25 18:34:51 -0700

    MrLister setting sass 4-25-17 6:34pm

--- template-parts/navigation/nav-main.php
+++ template-parts/navigation/nav-main.php
@@ -13,14 +13,15 @@
 <nav class="navbar navbar-default w-nav-color" role="navigation">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
+    <div class="navbar-header text-center">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary_walker">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>">
-                <?php bloginfo('name'); ?> <div class="hidden-md hidden-lg w-brand-logo"><?php the_custom_logo(); ?></div>
+      <a type="button" class="hidden-md hidden-lg navbar-brand w-back-button" onclick="history.back();"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span> Back</a>
+      <a class="navbar-brand w-blog-brand" href="<?php echo home_url(); ?>">
+                <?php bloginfo('name'); ?>
             </a>
     </div>
   <?php wp_nav_menu( array( 
